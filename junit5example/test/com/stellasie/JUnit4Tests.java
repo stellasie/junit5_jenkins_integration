4deff8066d0c50455cd8be4517874f5a5fcde321
stellasie <stellasie@yahoo.com>
2016-08-27 15:31:55 +0200

    Add Junit 5 and original JUnit 4 tests

---
+++ junit5example/test/com/stellasie/JUnit4Tests.java
@@ -0,0 +1,15 @@
+package com.stellasie;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Created by gaming on 2016/08/27.
+ */
+public class JUnit4Tests {
+    @Test
+    public void junit4Tests() {
+        assertTrue(true);
+    }
+
+}
---
+++ junit5example/test/com/stellasie/JUnit5Tests.java
@@ -0,0 +1,12 @@
+package com.stellasie;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("JUnit 5 Tests")
+public class JUnit5Tests {
+    @Test
+    void displayNameTest() {
+
+    }
+}
